910bc6dc902229ae553de29775e47ca0bbd4df38
Biel35 <83042989+Gabrico45@users.noreply.github.com>
2025-04-29 11:15:39 -0300

    Update desafioNaval.c

--- desafioNaval.c
+++ desafioNaval.c
@@ -1,148 +1,175 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define linha 10 
-#define coluna 10
+#define LINHA 10 
+#define COLUNA 10
+#define TAM_HAB 5 // Tamanho da matriz da habilidade (5x5)
 
-int main ()
-{
-    int tabuleiro [linha][coluna] = {0};
+// Função para exibir o tabuleiro |  Lembrando que tive que alterar a agua de zero para '~' e navio virou 'N' tudo isso pra poder adicionar os poderes. 
+void exibirTabuleiro(int tabuleiro[LINHA][COLUNA]) {
+    printf("\n=== TABULEIRO ===\n");
+    for (int i = 0; i < LINHA; i++) {
+        for (int j = 0; j < COLUNA; j++) {
+            if (tabuleiro[i][j] == 0)
+                printf("~ ");
+            else if (tabuleiro[i][j] == 3)
+                printf("N ");
+            else if (tabuleiro[i][j] == 5)
+                printf("* ");
+            else
+                printf("? ");
+        }
+        printf("\n");
+    }
+}
 
-    int naV;
-    int naH;
+// Função para aplicar habilidade
+void aplicarHabilidade(int tabuleiro[LINHA][COLUNA], int origem_linha, int origem_coluna, char tipo) {
+    int habilidade[TAM_HAB][TAM_HAB] = {0};
+    int centro = TAM_HAB / 2;
+
+    for (int i = 0; i < TAM_HAB; i++) {
+        for (int j = 0; j < TAM_HAB; j++) {
+            switch (tipo) {
+                case 'c': // Cone
+                    if (i >= j - centro && i >= centro - j && i <= centro)
+                        habilidade[i][j] = 1;
+                    break;
+                case 'r': // Cruz
+                    if (i == centro || j == centro)
+                        habilidade[i][j] = 1;
+                    break;
+                case 'o': // Octaedro
+                    if (abs(i - centro) + abs(j - centro) <= centro)
+                        habilidade[i][j] = 1;
+                    break;
+            }
+        }
+    }
+// Aqui eu pego cada célula da matriz de habilidade e calculo onde ela cai no tabuleiro, centralizada em (origem_linha, origem_coluna). | Verifica se está dentro dos limites do tabuleiro (para não acessar posições inválidas).
+    for (int i = 0; i < TAM_HAB; i++) {
+        for (int j = 0; j < TAM_HAB; j++) {
+            int linhaTab = origem_linha + i - centro;
+            int colunaTab = origem_coluna + j - centro;
+
+            if (linhaTab >= 0 && linhaTab < LINHA && colunaTab >= 0 && colunaTab < COLUNA) {
+                if (habilidade[i][j] == 1 && tabuleiro[linhaTab][colunaTab] == 0)
+                    tabuleiro[linhaTab][colunaTab] = 5;
+            }
+        }
+    }
+}
+
+int main() {
+    int tabuleiro[LINHA][COLUNA] = {0};
+    int naV, naH;
     char direcao;
 
     printf("--- BATALHA NAVAL ---\n");
-    printf("Escola a posição do navio: 'h' para horizontal, 'v' para vertical, 'd' para diagonal direita ou 'e' para diagonal esquerda: : ");
-    scanf(" %c", &direcao); // espaço antes do %c para ignorar enter pendente
+    printf("Escolha a posição do navio: 'h' para horizontal, 'v' para vertical, 'd' para diagonal direita ou 'e' para diagonal esquerda: ");
+    scanf(" %c", &direcao);
 
-    // codigo para identificar erro de digito.
-    while (direcao != 'h' && direcao != 'v' && direcao != 'd' && direcao != 'e')
-    {
-        printf("Direção inválida. Digite 'h' para horizontal, 'v' para vertical, 'd' para diagonal direita ou 'e' para diagonal esquerda: \n");
+    while (direcao != 'h' && direcao != 'v' && direcao != 'd' && direcao != 'e') {
+        printf("Direção inválida. Digite 'h', 'v', 'd' ou 'e': ");
         scanf(" %c", &direcao);
     }
 
-    //direcao para vertical 
-    if (direcao == 'v')
-    {
-        printf("Digite a linha do navio vertical (0 a %d): ", linha - 3);
+    if (direcao == 'v') {
+        printf("Digite a linha do navio vertical (0 a %d): ", LINHA - 3);
         scanf("%d", &naV);
-
-        //esses codigos em while são para identificar algum erro e repitir até que o usuario escolha o numero ou digito correto.
-        while (naV < 0 || naV > linha - 3)
-        {
-            printf("Linha Inválida. Digite novamente (0 a %d): \n", linha - 3);
+        while (naV < 0 || naV > LINHA - 3) {
+            printf("Linha inválida. Digite novamente (0 a %d): ", LINHA - 3);
             scanf("%d", &naV);
         }
 
-        printf("Digite a coluna do navio vertical (0 a %d): ", coluna - 1);
+        printf("Digite a coluna do navio vertical (0 a %d): ", COLUNA - 1);
         scanf("%d", &naH);
-
-        while (naH < 0 || naH >= coluna)
-        {
-            printf("Coluna inválida. Digite novamente (0 a %d): \n", coluna - 1);
+        while (naH < 0 || naH >= COLUNA) {
+            printf("Coluna inválida. Digite novamente (0 a %d): ", COLUNA - 1);
             scanf("%d", &naH);
         }
 
-        printf("\n--- CORDENADAS ---\n");
         for (int i = 0; i < 3; i++)
-        {
             tabuleiro[naV + i][naH] = 3;
-            printf("Coordenada do Navio Vertical: (%d, %d)\n", naV + i, naH);
-        }
-    }
-    
-    //direcao horizontal.
-    else if (direcao == 'h')
-    {
-        printf("Digite a linha do navio horizontal (0 a %d): \n", linha - 1);
-        scanf("%d", &naV);
-
-        while (naV < 0 || naV >= linha)
-        {
-            printf("Linha inválida. Digite novamente (0 a %d): \n", linha - 1);
-            scanf("%d", &naV);
-        }
-
-        printf("Digite a Coluna do navio horizontal (0 a %d): ", coluna - 3);
-        scanf("%d", &naH);
-
-        while (naH < 0 || naH > coluna - 3)
-        {
-            printf("Coluna inválida. Digite novamente (0 a %d): \n", coluna - 3);
-            scanf("%d", &naH);
-        }
-
-        printf("\n--- CORDENADAS ---\n");
-        for (int i = 0; i < 3; i++)
-        {
-            tabuleiro[naV][naH + i] = 3;
-            printf("Coordenada do navio Horizontal: (%d, %d)\n", naV, naH + i);
-        }
-    }
-    
-    else if (direcao == 'd') // diagonal direita "\"
-    {
-        printf("Digite a linha do navio diagonal direita (0 a %d): ", linha - 3);
-        scanf("%d", &naV);
-
-        while (naV < 0 || naV > linha - 3)
-        {
-            printf("Linha inválida. Digite novamente (0 a %d): ", linha - 3);
-            scanf("%d", &naV);
-        }
-
-        printf("Digite a coluna do navio diagonal direita (0 a %d): ", coluna - 3);
-        scanf("%d", &naH);
-
-        while (naH < 0 || naH > coluna - 3)
-        {
-            printf("Coluna inválida. Digite novamente (0 a %d): ", coluna - 3);
-            scanf("%d", &naH);
-        }
-
-        printf("\n--- CORDENADAS ---\n");
-        for (int i = 0; i < 3; i++)
-        {
-            tabuleiro[naV + i][naH + i] = 3;
-            printf("Coordenada do navio Diagonal Direita: (%d, %d)\n", naV + i, naH + i);
-        }
-    }
-    
-    else if (direcao == 'e') // diagonal esquerda "/"
-    {
-        printf("Digite a linha do navio diagonal esquerda (0 a %d): ", linha - 3);
-        scanf("%d", &naV);
-
-        while (naV < 0 || naV > linha - 3)
-        {
-            printf("Linha inválida. Digite novamente (0 a %d): ", linha - 3);
-            scanf("%d", &naV);
-        }
-
-        printf("Digite a coluna do navio diagonal esquerda (2 a %d): ", coluna - 1);
-        scanf("%d", &naH);
-
-        while (naH < 2 || naH > coluna - 1)
-        {
-            printf("Coluna inválida. Digite novamente (2 a %d): ", coluna - 1);
-            scanf("%d", &naH);
-        }
-
-        printf("\n--- CORDENADAS ---\n");
-        for (int i = 0; i < 3; i++)
-        {
-            tabuleiro[naV + i][naH - i] = 3;
-            printf("Coordenada do navio Diagonal Esquerda: (%d, %d)\n", naV + i, naH - i);
-        }
-    }
-
-    // Exibição do tabuleiro
-    printf("\n=== TABULEIRO ===\n");
-    for (int i = 0; i < linha; i++) {
-        for (int j = 0; j < coluna; j++) {
-            printf("%d ", tabuleiro[i][j]);
-        }
-        printf("\n");
     } 
+    else if (direcao == 'h') {
+        printf("Digite a linha do navio horizontal (0 a %d): ", LINHA - 1);
+        scanf("%d", &naV);
+        while (naV < 0 || naV >= LINHA) {
+            printf("Linha inválida. Digite novamente (0 a %d): ", LINHA - 1);
+            scanf("%d", &naV);
+        }
+
+        printf("Digite a coluna do navio horizontal (0 a %d): ", COLUNA - 3);
+        scanf("%d", &naH);
+        while (naH < 0 || naH > COLUNA - 3) {
+            printf("Coluna inválida. Digite novamente (0 a %d): ", COLUNA - 3);
+            scanf("%d", &naH);
+        }
+
+        for (int i = 0; i < 3; i++)
+            tabuleiro[naV][naH + i] = 3;
+    } 
+    else if (direcao == 'd') {
+        printf("Digite a linha do navio diagonal direita (0 a %d): ", LINHA - 3);
+        scanf("%d", &naV);
+        while (naV < 0 || naV > LINHA - 3) {
+            printf("Linha inválida. Digite novamente (0 a %d): ", LINHA - 3);
+            scanf("%d", &naV);
+        }
+
+        printf("Digite a coluna do navio diagonal direita (0 a %d): ", COLUNA - 3);
+        scanf("%d", &naH);
+        while (naH < 0 || naH > COLUNA - 3) {
+            printf("Coluna inválida. Digite novamente (0 a %d): ", COLUNA - 3);
+            scanf("%d", &naH);
+        }
+
+        for (int i = 0; i < 3; i++)
+            tabuleiro[naV + i][naH + i] = 3;
+    } 
+    else if (direcao == 'e') {
+        printf("Digite a linha do navio diagonal esquerda (0 a %d): ", LINHA - 3);
+        scanf("%d", &naV);
+        while (naV < 0 || naV > LINHA - 3) {
+            printf("Linha inválida. Digite novamente (0 a %d): ", LINHA - 3);
+            scanf("%d", &naV);
+        }
+
+        printf("Digite a coluna do navio diagonal esquerda (2 a %d): ", COLUNA - 1);
+        scanf("%d", &naH);
+        while (naH < 2 || naH > COLUNA - 1) {
+            printf("Coluna inválida. Digite novamente (2 a %d): ", COLUNA - 1);
+            scanf("%d", &naH);
+        }
+
+        for (int i = 0; i < 3; i++)
+            tabuleiro[naV + i][naH - i] = 3;
+    }
+
+    // Exibir tabuleiro após navio
+    exibirTabuleiro(tabuleiro);
+
+    // Aplicar habilidade
+    char tipoHab;
+    int origem_linha, origem_coluna;
+
+    printf("\nDigite o tipo de habilidade: 'c' para cone, 'r' para cruz, 'o' para octaedro: ");
+    scanf(" %c", &tipoHab);
+    while (tipoHab != 'c' && tipoHab != 'r' && tipoHab != 'o') {
+        printf("Tipo inválido. Digite novamente ('c', 'r', 'o'): ");
+        scanf(" %c", &tipoHab);
+    }
+
+    printf("Digite a linha de origem da habilidade (0 a %d): ", LINHA - 1);
+    scanf("%d", &origem_linha);
+    printf("Digite a coluna de origem da habilidade (0 a %d): ", COLUNA - 1);
+    scanf("%d", &origem_coluna);
+
+    aplicarHabilidade(tabuleiro, origem_linha, origem_coluna, tipoHab);
+
+    // Exibir tabuleiro final
+    exibirTabuleiro(tabuleiro);
+
+    return 0;
 }
